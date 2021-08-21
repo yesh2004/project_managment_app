d7b1015417a3793a5a0832890be36fac8ee0a9eb
Yeshwanth Kumar <v.yeshwanth2004@gmail.com>
2021-08-21 19:20:57 +0300

    monday.com clone setup

---
+++ backend/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Project(models.Model):
+	project_name=models.CharField(max_length=500)
+	owner=models.ForeignKey(User,on_delete=models.CASCADE)
+	date_created=models.DateField(auto_now=True)
+
+class Row(models.Model):
+	priority_choices=(
+		("Low","low"),
+		("Meduim","meduim"),
+		("High","high")
+		)
+	completed_choices=(
+		("Completed","completed"),
+		("Inprogress","inprogress"),
+		("Not Started","not_started"))
+	project=models.ForeignKey(Project,on_delete=models.CASCADE)
+	priority=models.CharField(max_length=50,choices=priority_choices,default="Low")
+	task_name=models.CharField(max_length=300)
+	assigned_to=models.CharField(max_length=300)
+	status=models.CharField(max_length=50,choices=completed_choices,default="Not Started")
+	deadline=models.DateField(auto_now=False)
+
